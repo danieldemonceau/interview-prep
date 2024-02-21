95cde61f7648b5cf8f3b341e0e5b8d340df2c93a
Daniel Demonceau <daniel.demonceau@directed.com.au>
2024-02-21 15:59:31 +0700

    0022 generate-parentheses

--- code-challenges/leetcode/0022.generate-parentheses.ts
+++ code-challenges/leetcode/0022.generate-parentheses.ts
@@ -1,19 +1,13 @@
 const generateParenthesis = (n: number): string[] => {
   const solutions: string[] = [];
-  const generateParenthesisHelper = (
-    numOfOpened = 0,
-    numOfClosed = 0,
-    path = "",
-  ) => {
+  const generateParenthesisHelper = (numOfOpened = 0, numOfClosed = 0, path = '') => {
     if (numOfClosed === n) {
       solutions.push(path);
       return;
     }
-    if (numOfOpened < n)
-      generateParenthesisHelper(numOfOpened + 1, numOfClosed, path + "(");
-    if (numOfClosed < numOfOpened)
-      generateParenthesisHelper(numOfOpened, numOfClosed + 1, path + ")");
-  };
+    if (numOfOpened < n) generateParenthesisHelper(numOfOpened + 1, numOfClosed, path + '(');
+    if (numOfClosed < numOfOpened) generateParenthesisHelper(numOfOpened, numOfClosed + 1, path + ')');
+  }
   generateParenthesisHelper();
   return solutions;
 };
