bf541368b028faa97bc9f9db5f0e8332de9b7f2d
Daniel Demonceau <daniel.demonceau@directed.com.au>
2024-02-22 14:48:21 +0700

    0024 swap-nodes-in-pairs

---
+++ code-challenges/leetcode/0024.swap-nodes-in-pairs.ts
@@ -0,0 +1,23 @@
+import { ListNode } from "./classes/ListNode";
+
+const swapPairs = (head: ListNode | null): ListNode | null => {
+  const dummyHead = new ListNode(0, head);
+  let previousNode: ListNode = dummyHead;
+  let currentNode: ListNode | null = head;
+  while (currentNode && currentNode.next) {
+    const nextCurrentNode: ListNode | null = currentNode.next.next;
+    const nextPreviousNode = currentNode.next;
+
+    nextPreviousNode.next = currentNode;
+    currentNode.next = nextCurrentNode;
+    previousNode.next = nextPreviousNode;
+
+    previousNode = currentNode;
+    currentNode = nextCurrentNode;
+  }
+  return dummyHead.next;
+};
+
+console.log(swapPairs(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))))));
+console.log(swapPairs(new ListNode()));
+console.log(swapPairs(new ListNode(1)));
