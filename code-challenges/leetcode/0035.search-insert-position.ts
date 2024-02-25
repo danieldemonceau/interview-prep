1581cec8dc7b2c7d806a71ac815e64fd2b9bea85
Daniel Demonceau <daniel.demonceau@directed.com.au>
2024-02-25 15:01:22 +0700

    0035 search-insert-position

--- code-challenges/leetcode/0035.search-insert-position.ts
+++ code-challenges/leetcode/0035.search-insert-position.ts
@@ -13,3 +13,4 @@ const searchInsert = (nums: number[], target: number): number => {
 console.log(searchInsert([1, 3, 5, 6], 5));
 console.log(searchInsert([1, 3, 5, 6], 2));
 console.log(searchInsert([1, 3, 5, 6], 7));
+
