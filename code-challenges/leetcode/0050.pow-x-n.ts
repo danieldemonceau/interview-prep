62008d07eb69ab70f0dff158b94f417a7bcd8576
danieldemonceau <daniel.demonceau@yahoo.com>
2024-03-01 09:24:42 +0700

    0050 pow-x-n

---
+++ code-challenges/leetcode/0050.pow-x-n.ts
@@ -0,0 +1,10 @@
+const myPow = (x: number, n: number): number => {
+  if (n === 0) return 1;
+  if (n < 0) return 1 / myPow(x, -n);
+  if (n % 2) return x * myPow(x, n - 1);
+  return myPow(x * x, n / 2);
+};
+
+console.log(myPow(2, 10));
+console.log(myPow(2.1, 3));
+console.log(myPow(2, -2));
