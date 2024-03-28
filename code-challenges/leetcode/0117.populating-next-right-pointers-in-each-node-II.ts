fc7fd1c77a4b9749287044544409f121f0440f35
Daniel Georges Demonceau <daniel.demonceau@yahoo.com>
2024-03-28 22:38:49 +1100

    0117 populating-next-right-pointers-in-each-node-II

---
+++ code-challenges/leetcode/0117.populating-next-right-pointers-in-each-node-II.ts
@@ -0,0 +1,29 @@
+import { Node } from "./classes/Node";
+
+const connect = (root: Node | null): Node | null => {
+  if (!root) return null;
+  let nodes: Node[] = [root];
+  while (nodes.length > 0) {
+    const newNodes: Node[] = [];
+    let prevNode: Node | null = null;
+    for (const node of nodes) {
+      if (prevNode) prevNode.next = node;
+      if (node.left) newNodes.push(node.left);
+      if (node.right) newNodes.push(node.right);
+      prevNode = node;
+    }
+    nodes = newNodes;
+  }
+  return root;
+};
+
+console.log(
+  connect(
+    new Node(
+      1,
+      new Node(2, new Node(4), new Node(5)),
+      new Node(3, undefined, new Node(7)),
+    ),
+  ),
+);
+console.log(connect(null));
