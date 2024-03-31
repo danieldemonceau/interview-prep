f67045a8a680e45a40e8a4ebfaaceccb84a64465
Daniel Georges Demonceau <daniel.demonceau@yahoo.com>
2024-03-31 13:27:56 +1100

    0124 binary-tree-maximum-path-sum

---
+++ code-challenges/leetcode/0124.binary-tree-maximum-path-sum.ts
@@ -0,0 +1,28 @@
+import { TreeNode } from "./classes/TreeNode";
+
+const maxPathSum = (root: TreeNode | null): number => {
+  const maxPathSumHelper = (root: TreeNode | null): [number, number] => {
+    if (!root) return [-Infinity, 0];
+    const [leftMaxSum, leftMaxRoot] = maxPathSumHelper(root.left);
+    const [rightMaxSum, rightMaxRoot] = maxPathSumHelper(root.right);
+    const maxRoot = root.val + Math.max(0, leftMaxRoot, rightMaxRoot);
+    const maxSum = Math.max(
+      leftMaxSum,
+      rightMaxSum,
+      leftMaxRoot + root.val + rightMaxRoot,
+    );
+    return [maxSum, maxRoot];
+  };
+  return maxPathSumHelper(root)[0];
+};
+
+console.log(maxPathSum(new TreeNode(1, new TreeNode(2), new TreeNode(3))));
+console.log(
+  maxPathSum(
+    new TreeNode(
+      -10,
+      new TreeNode(9),
+      new TreeNode(20, new TreeNode(15), new TreeNode(7)),
+    ),
+  ),
+);
