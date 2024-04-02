740cc841d4369e09b7210cd0149cc1d438e3fc51
Daniel Georges Demonceau <daniel.demonceau@yahoo.com>
2024-04-02 23:57:48 +1100

    0136 single-number

---
+++ code-challenges/leetcode/0136.single-number.ts
@@ -0,0 +1,7 @@
+const singleNumber = (nums: number[]): number => {
+  return 0;
+};
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
