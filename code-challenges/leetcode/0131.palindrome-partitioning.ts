a71ecf9cd1031e00afe44ebe026e5e11a2c91066
Daniel Georges Demonceau <daniel.demonceau@yahoo.com>
2024-04-10 00:58:08 +1000

    0131 palindrome-partitioning

--- code-challenges/leetcode/0131.palindrome-partitioning.ts
+++ code-challenges/leetcode/0131.palindrome-partitioning.ts
@@ -1,4 +1,8 @@
-const partition = (s: string): string[][] => {};
+const partition = (s: string): string[][] => {
+  if (s.length === 1) return [[s]];
+  console.log("TODO");
+  return [];
+};
 
 console.log(partition("aab"));
 console.log(partition("a"));
