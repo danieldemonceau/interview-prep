92e3cce25c440b5c484a1d12fafbfe32314bcbd0
Daniel Georges Demonceau <daniel.demonceau@yahoo.com>
2024-04-24 21:04:08 +1000

    0168 excel-sheet-column-title

--- code-challenges/leetcode/0168.excel-sheet-column-title.ts
+++ code-challenges/leetcode/0168.excel-sheet-column-title.ts
@@ -1,5 +1,16 @@
-const convertToTitle = (columnNumber: number): string => {};
+const convertToTitle = (columnNumber: number): string => {
+  let result = "";
+  while (columnNumber > 0) {
+    columnNumber--;
+    result = String.fromCharCode((columnNumber % 26) + 65) + result;
+    columnNumber = Math.floor(columnNumber / 26);
+  }
+  return result;
+};
 
 console.log(convertToTitle(1));
 console.log(convertToTitle(28));
 console.log(convertToTitle(701));
+
+console.log(convertToTitle(26));
+console.log(convertToTitle(27));
